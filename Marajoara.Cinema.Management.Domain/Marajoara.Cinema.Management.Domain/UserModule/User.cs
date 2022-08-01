6d452a5363ecf03dfa423307aaab58c2aa8d5471
Rafael Polmann <rafaelpolmann@gmail.com>
2022-08-01 15:31:32 -0300

    Criando estrutura da solução no repositório (GIT) da aplicação.

---
+++ Marajoara.Cinema.Management.Domain/Marajoara.Cinema.Management.Domain/UserModule/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Marajoara.Cinema.Management.Domain.UserModule
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+
+    }
+}
