3de5c5af4fdee88df444681a162484f0c85fe7c3
Akauam <akauam@hotmail.com>
2022-08-04 11:42:26 -0300

    Criado configuration para os objetos de dominio: Movie, Session, Ticket, UserAccount;

--- Marajoara.Cinema.Management/Marajoara.Cinema.Management.Infra.Data.EF/Configuration/MovieConfiguration.cs
+++ Marajoara.Cinema.Management/Marajoara.Cinema.Management.Infra.Data.EF/Configuration/MovieConfiguration.cs
@@ -3,7 +3,19 @@ using System.Data.Entity.ModelConfiguration;
 
 namespace Marajoara.Cinema.Management.Infra.Data.EF.Configuration
 {
-    class MovieConfiguration : EntityTypeConfiguration<Movie>
+    public class MovieConfiguration : EntityTypeConfiguration<Movie>
     {
+        public MovieConfiguration()
+        {
+            ToTable("Movies");
+            HasKey(cr => cr.MovieID).Property(cr => cr.MovieID).HasColumnName("MovieID");
+            HasIndex(cr => cr.Title).IsUnique();
+            Property(cr => cr.Title).IsRequired().HasColumnName("Title").HasMaxLength(1024);
+            Property(cr => cr.Poster).HasColumnName("Poster");
+            Property(cr => cr.Duration).IsRequired().HasColumnName("Duration");
+            Property(cr => cr.Is3D).IsRequired().HasColumnName("Is3D");
+            Property(cr => cr.IsOrignalAudio).IsRequired().HasColumnName("IsOrignalAudio");
+            
+        }
     }
 }
