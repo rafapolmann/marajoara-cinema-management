266dd6e8ef2de236a1083943b6a022c7f857511d
Akauam <akauam@hotmail.com>
2022-08-10 16:57:21 -0300

    Added IUserAccountRepository with basic methods

---
+++ Marajoara.Cinema.Management/Marajoara.Cinema.Management.Domain/UserAccountModule/IUserAccountRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marajoara.Cinema.Management.Domain.UserAccountModule
+{
+    public interface IUserAccountRepository
+    {
+        void Add(UserAccount userAccountToAdd);
+        void Update(UserAccount userAccountToUpdate);
+        void Delete(UserAccount userAccountToDelete);
+        IEnumerable<UserAccount> RetriveAll();
+
+        /// <summary>
+        /// Retrieves a UserAccount with a given mail
+        /// </summary>
+        /// <param name="userAccountID"></param>
+        /// <returns></returns>
+        UserAccount RetrieveByMail(string userAccountMail);
+    }
+}
