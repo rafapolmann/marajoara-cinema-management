42fabf4784f64840059397a4cd65dc02525bdd7a
Rafael Polmann <rafaelpolmann@gmail.com>
2022-08-10 20:01:49 -0300

    - adjusting interface IMarajoaraUnitOfWork           add: IMovieRepository                    ISessionRepository                    ITicketRepository

--- Marajoara.Cinema.Management/Marajoara.Cinema.Management.Domain/UnitOfWork/IMarajoaraUnitOfWork.cs
+++ Marajoara.Cinema.Management/Marajoara.Cinema.Management.Domain/UnitOfWork/IMarajoaraUnitOfWork.cs
@@ -1,4 +1,7 @@
 ﻿using Marajoara.Cinema.Management.Domain.CineRoomModule;
+using Marajoara.Cinema.Management.Domain.MovieModule;
+using Marajoara.Cinema.Management.Domain.SessionModule;
+using Marajoara.Cinema.Management.Domain.TicketModule;
 using Marajoara.Cinema.Management.Domain.UserAccountModule;
 using System;
 
@@ -8,6 +11,9 @@ namespace Marajoara.Cinema.Management.Domain.UnitOfWork
     {
         public IUserAccountRepository UserAccounts { get; }
         public ICineRoomRepository CineRooms { get; }
+        public IMovieRepository Movies { get; }
+        public ISessionRepository Sessions { get; }
+        public ITicketRepository Tickets { get; }
         void Commit();
         void CleanDb();
     }
