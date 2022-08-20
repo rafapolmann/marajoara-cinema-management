3926d895fd471c448e45b5f9ea5a1080741ae494
Rafael Polmann <rafaelpolmann@gmail.com>
2022-08-20 19:59:12 -0300

    Implemented MovieService RemoveMovie

--- Marajoara.Cinema.Management/Marajoara.Cinema.Management.Application/Features/MovieModule/MovieService.cs
+++ Marajoara.Cinema.Management/Marajoara.Cinema.Management.Application/Features/MovieModule/MovieService.cs
@@ -1,6 +1,8 @@
 ﻿using Marajoara.Cinema.Management.Domain.MovieModule;
 using Marajoara.Cinema.Management.Domain.UnitOfWork;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Marajoara.Cinema.Management.Application.Features.MovieModule
 {
@@ -14,14 +16,28 @@ namespace Marajoara.Cinema.Management.Application.Features.MovieModule
 
         public int AddMovie(Movie movie)
         {
-            //2.Filmes não podem ter nomes repetidos - validação deve ocorrer em tempo
-            //real sem que haja necessidade de submeter o formulário;
+            //2.Filmes não podem ter nomes repetidos
             throw new System.NotImplementedException();
         }
 
         public bool RemoveMovie(Movie movie)
         {
-            throw new System.NotImplementedException();
+            if (movie == null)
+                throw new ArgumentException("Movie parameter cannot be null.", nameof(movie));
+
+            Movie movieToDelete = movie.MovieID > 0 ?
+                                        _unitOfWork.Movies.Retrieve(movie.MovieID) :
+                                        _unitOfWork.Movies.RetrieveByTitle(movie.Title);
+
+            if (movieToDelete == null)
+                throw new Exception($"Movie not found.");
+            if (_unitOfWork.Sessions.RetrieveByMovieTitle(movieToDelete.Title).ToList().Count > 0)
+                throw new Exception($"Cannot possible remove movie {movieToDelete.Title}. There are sessions linked with this movie.");
+
+            _unitOfWork.Movies.Delete(movieToDelete);
+            _unitOfWork.Commit();
+
+            return true;
         }
 
         public IEnumerable<Movie> GetAllMovies()
