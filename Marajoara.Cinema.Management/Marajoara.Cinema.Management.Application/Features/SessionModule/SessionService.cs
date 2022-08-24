d8188478f2add48740db4e0187782eab416e4961
Rafael Polmann <rafaelpolmann@gmail.com>
2022-08-24 17:51:20 -0300

    .

--- Marajoara.Cinema.Management/Marajoara.Cinema.Management.Application/Features/SessionModule/SessionService.cs
+++ Marajoara.Cinema.Management/Marajoara.Cinema.Management.Application/Features/SessionModule/SessionService.cs
@@ -1,4 +1,6 @@
-﻿using Marajoara.Cinema.Management.Domain.SessionModule;
+﻿using Marajoara.Cinema.Management.Domain.CineRoomModule;
+using Marajoara.Cinema.Management.Domain.MovieModule;
+using Marajoara.Cinema.Management.Domain.SessionModule;
 using Marajoara.Cinema.Management.Domain.UnitOfWork;
 using System.Collections.Generic;
 
@@ -7,9 +9,30 @@ namespace Marajoara.Cinema.Management.Application.Features.SessionModule
     public class SessionService : ISessionService
     {
         private readonly IMarajoaraUnitOfWork _unitOfWork;
-        public SessionService(IMarajoaraUnitOfWork unitOfWork)
+        private readonly ICineRoomService _cineRoomService;
+        private readonly IMovieService _movieService;
+        public SessionService(IMarajoaraUnitOfWork unitOfWork, ICineRoomService cineRoomService, IMovieService movieService)
         {
             _unitOfWork = unitOfWork;
+            _cineRoomService = cineRoomService;
+            _movieService = movieService;
+        }
+
+        public int AddSession(Session session)
+        {
+            CineRoom sessionCineRoom = _cineRoomService.GetCineRoom(session.CineRoomID);
+            //validar se tem sala válida
+            //validar se ja não existe sessão na sala no mesmo horario (horario sessão + duração do filme)
+            session.CineRoom = sessionCineRoom;
+
+            Movie sessionMovie = _movieService.GetMovie(session.MovieID);
+            //validar se tem filme valido
+            session.Movie = sessionMovie;
+
+            _unitOfWork.Sessions.Add(session);
+            _unitOfWork.Commit();
+
+            return session.SessionID;
         }
 
         public Session GetSession(int id)
--- Marajoara.Cinema.Management/Marajoara.Cinema.Management.Domain/SessionModule/ISessionService.cs
+++ Marajoara.Cinema.Management/Marajoara.Cinema.Management.Domain/SessionModule/ISessionService.cs
@@ -5,6 +5,16 @@ namespace Marajoara.Cinema.Management.Domain.SessionModule
     public interface ISessionService
     {
         /// <summary>
+        /// Add new session in the system.
+        /// In case of the session parameter is null, will throw exception.
+        /// Will not possible to register a new session without a valid CineRoom and a valid Movie.
+        /// Will not possible add a session in the CineRoom taht already existing a session in the same time.
+        /// </summary>
+        /// <param name="movie">Session to add.</param>
+        /// <returns>Return the ID of new session registered in the system.</returns>
+        int AddSession(Session session);
+
+        /// <summary>
         /// Method to get a given Session registered on the system based on database ID.
         /// </summary>
         /// <param name="id">ID used with parameter in the search.</param>
