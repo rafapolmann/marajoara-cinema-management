7634a59330c88d2c604eb3b17250dec6c39ffc03
Akauam <akauam@hotmail.com>
2022-08-30 15:36:46 -0300

    Enabled token bearer for the app Swagger.

--- Marajoara.Cinema.Management/Marajoara.Cinema.Management.Api/Startup.cs
+++ Marajoara.Cinema.Management/Marajoara.Cinema.Management.Api/Startup.cs
@@ -12,17 +12,19 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Marajoara.Cinema.Management.Api
 {
     public class Startup
     {
-        private  ITokenService _tokenService;
+        private ITokenService _tokenService;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
             //Todo: check if there is a better way of get the instance of this service
-            _tokenService =  IoC.GetInstance().Get<ITokenService>();
+            _tokenService = IoC.GetInstance().Get<ITokenService>();
 
         }
 
@@ -50,13 +52,43 @@ namespace Marajoara.Cinema.Management.Api
 
             services.Configure<ApiBehaviorOptions>(options => { options.SuppressModelStateInvalidFilter = true; });
             services.AddControllers(opt => { opt.Filters.Add(typeof(ValidatorActionFilter)); });
-            
+
             services.AddValidatorsFromAssembly(typeof(Startup).Assembly);
             services.AddFluentValidationAutoValidation();
 
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Marajoara.Cinema.Management.Api", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                    Reference = new OpenApiReference
+                        {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header,
+
+                    },
+                    new List<string>()
+                    }
+                });
+
             });
         }
 
@@ -73,7 +105,7 @@ namespace Marajoara.Cinema.Management.Api
             app.UseHttpsRedirection();
 
             app.UseRouting();
-
+            app.UseCors();
             app.UseAuthentication();
             app.UseAuthorization();
 
