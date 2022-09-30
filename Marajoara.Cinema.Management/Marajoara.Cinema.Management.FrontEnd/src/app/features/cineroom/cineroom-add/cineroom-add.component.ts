e71a71e44a427977c3e318db713ad82385d48fcd
Akauam <akauam@hotmail.com>
2022-09-30 14:36:29 -0300

    Snackbar notification on cineroom-add: - Added the snackbar notification when there is an error return from de server

--- Marajoara.Cinema.Management/Marajoara.Cinema.Management.FrontEnd/src/app/features/cineroom/cineroom-add/cineroom-add.component.ts
+++ Marajoara.Cinema.Management/Marajoara.Cinema.Management.FrontEnd/src/app/features/cineroom/cineroom-add/cineroom-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { firstValueFrom } from 'rxjs';
 import { CineRoom } from 'src/app/models/CineRoom';
@@ -7,37 +8,49 @@ import { CineRoomService } from 'src/app/services/CineRoomService';
 @Component({
   selector: 'app-cineroom-add',
   templateUrl: './cineroom-add.component.html',
-  styleUrls: ['./cineroom-add.component.scss']
+  styleUrls: ['./cineroom-add.component.scss'],
 })
 export class CineroomAddComponent implements OnInit {
-  constructor(private cineRoomService: CineRoomService, private router: Router) {}
+  constructor(
+    private cineRoomService: CineRoomService,
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
 
   ngOnInit(): void {}
-  
-  navigateToList(){
+
+  navigateToList() {
     this.router.navigate(['/cinerooms']);
   }
-  
+
   onSubmit(cineRoom: CineRoom) {
     this.saveMovie(cineRoom);
   }
 
-  onCancel(){
+  onCancel() {
     this.navigateToList();
   }
- 
+
   async saveMovie(cineRoom: CineRoom) {
     try {
-      cineRoom.cineRoomID = await firstValueFrom(this.cineRoomService.add(cineRoom));      
-    } catch (exception: any) {
-      console.log(exception);
-      alert(
-        `error status ${exception.status}; Message: ${
-          Object.values(exception.error)[0]
-        }`
+      cineRoom.cineRoomID = await firstValueFrom(
+        this.cineRoomService.add(cineRoom)
       );
-    }finally{
+    } catch (exception: any) {
+      this.showErrorMessage(exception);
+    } finally {
       this.navigateToList();
     }
+  }
+
+  showErrorMessage(exception: any) {
+    this.snackBar.open(
+      `error status ${exception.status} - ${Object.values(exception.error)[0]}`,
+      'Fechar',
+      {
+        verticalPosition: 'top',
+        horizontalPosition: 'right',
+      }
+    );
   }
 }
