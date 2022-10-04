7816fb996db76e9a9ae282c079ec3e82a2659516
Akauam <akauam@hotmail.com>
2022-10-04 16:09:11 -0300

    Autocomplete bugfix

--- Marajoara.Cinema.Management/Marajoara.Cinema.Management.FrontEnd/src/app/components/session/session-form/session-form.component.ts
+++ Marajoara.Cinema.Management/Marajoara.Cinema.Management.FrontEnd/src/app/components/session/session-form/session-form.component.ts
@@ -1,5 +1,11 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, FormGroupDirective, Validators, } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormGroupDirective,
+  Validators,
+} from '@angular/forms';
 import { Session } from 'src/app/models/Session';
 import { firstValueFrom, Observable } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
@@ -8,8 +14,6 @@ import { MovieService } from 'src/app/services/MovieService';
 import { CineRoom } from 'src/app/models/CineRoom';
 import { CineRoomService } from 'src/app/services/CineRoomService';
 import { DateTimeCustomFormat } from 'src/app/core/pipes/date-time-custom-format';
-
-
 
 @Component({
   selector: 'app-session-form',
@@ -31,17 +35,44 @@ export class SessionFormComponent implements OnInit {
   filteredCineRooms!: Observable<CineRoom[]>;
   cineRooms: CineRoom[] = [];
 
-  constructor(private movieService: MovieService, private cineRoomService: CineRoomService) { }
+  constructor(
+    private movieService: MovieService,
+    private cineRoomService: CineRoomService
+  ) {}
 
   ngOnInit(): void {
     this.sessionForm = new FormGroup({
-      sessionID: new FormControl(this.sessionData ? this.sessionData.sessionID : ''),
-      sessionDate: new FormControl(this.sessionData ? this.sessionData.sessionDate : ''.toLocaleString(), [Validators.required]),
-      sessionTime: new FormControl(this.sessionData ? this.dateTimeCustom.transformToTime(this.sessionData.sessionDate) : '', [Validators.required]),
-      endSession: new FormControl(this.sessionData ? this.dateTimeCustom.transform(this.sessionData.endSession) : '', [Validators.required]),
-      price: new FormControl(this.sessionData ? this.sessionData.price.toFixed(2) : '0.00', [Validators.required]),
-      movieCtrl: new FormControl(this.sessionData ? this.sessionData.movie : '', [Validators.required,this.RequireMatch]),
-      cineRoomCtrl: new FormControl(this.sessionData ? this.sessionData.cineRoom : '', [Validators.required, this.RequireMatch]),
+      sessionID: new FormControl(
+        this.sessionData ? this.sessionData.sessionID : ''
+      ),
+      sessionDate: new FormControl(
+        this.sessionData ? this.sessionData.sessionDate : ''.toLocaleString(),
+        [Validators.required]
+      ),
+      sessionTime: new FormControl(
+        this.sessionData
+          ? this.dateTimeCustom.transformToTime(this.sessionData.sessionDate)
+          : '',
+        [Validators.required]
+      ),
+      endSession: new FormControl(
+        this.sessionData
+          ? this.dateTimeCustom.transform(this.sessionData.endSession)
+          : '',
+        [Validators.required]
+      ),
+      price: new FormControl(
+        this.sessionData ? this.sessionData.price.toFixed(2) : '0.00',
+        [Validators.required]
+      ),
+      movieCtrl: new FormControl(
+        this.sessionData ? this.sessionData.movie : '',
+        [Validators.required, this.RequireMatch]
+      ),
+      cineRoomCtrl: new FormControl(
+        this.sessionData ? this.sessionData.cineRoom : '',
+        [Validators.required, this.RequireMatch]
+      ),
     });
 
     this.loadMoviesInputFilter();
@@ -76,13 +107,17 @@ export class SessionFormComponent implements OnInit {
     this.movies = await firstValueFrom(this.movieService.getAll());
     this.filteredMovies = this.movieCtrl.valueChanges.pipe(
       startWith(''),
-      map((movie) => (movie ? this.filterMovies(movie.title) : this.movies.slice()))
+      map(() => this.filterMovies())
     );
   }
 
-  private filterMovies(value: string): Movie[] {
-    var hasNoValue =!value;
-    return hasNoValue?this.movies: this.movies.filter((movie) =>
+  private filterMovies(): Movie[] {
+    var value = this.movieCtrl.value;
+    value = typeof value === 'string' ? value : value!.title;
+
+    if (!value) return this.movies.slice();
+
+    return this.movies.filter((movie) =>
       movie.title.toLowerCase().includes(value.toLowerCase())
     );
   }
@@ -91,13 +126,17 @@ export class SessionFormComponent implements OnInit {
     this.cineRooms = await firstValueFrom(this.cineRoomService.getAll());
     this.filteredCineRooms = this.cineRoomCtrl.valueChanges.pipe(
       startWith(''),
-      map((cineRoom) => (cineRoom ? this.filterCineRooms(cineRoom.name) : this.cineRooms.slice()))
+      map(() => this.filterCineRooms())
     );
   }
 
-  private filterCineRooms(value: string): CineRoom[] {
-    var hasNoValue =!value;
-    return hasNoValue? this.cineRooms: this.cineRooms.filter((cineRoom) =>
+  private filterCineRooms(): CineRoom[] {
+    var value = this.cineRoomCtrl.value;
+    value = typeof value === 'string' ? value : value!.name;
+
+    if (!value) return this.cineRooms.slice();
+
+    return this.cineRooms.filter((cineRoom) =>
       cineRoom.name.toLowerCase().includes(value.toLowerCase())
     );
   }
@@ -116,34 +155,39 @@ export class SessionFormComponent implements OnInit {
     // this.sessionForm.reset();
     // formDirective.resetForm();
   }
-  endSessionChange(){
-    const movieDuration:number = this.movieCtrl.value.minutes;
-    const _date:Date = new Date(this.sessionDate.value);    
+  endSessionChange() {
+    const movieDuration: number = this.movieCtrl.value.minutes;
+    const _date: Date = new Date(this.sessionDate.value);
 
-    const time:number[] = this.sessionTime.value.split(':');    
+    const time: number[] = this.sessionTime.value.split(':');
 
-    const fullDate = new Date(_date.getFullYear(),_date.getMonth(),_date.getDate(), time[0], time[1],0,0);
-    
-    fullDate.setMinutes(fullDate.getMinutes() + movieDuration);    
+    const fullDate = new Date(
+      _date.getFullYear(),
+      _date.getMonth(),
+      _date.getDate(),
+      time[0],
+      time[1],
+      0,
+      0
+    );
+
+    fullDate.setMinutes(fullDate.getMinutes() + movieDuration);
     this.endSession.setValue(this.dateTimeCustom.transform(fullDate));
-
   }
 
-  movieAutoCompleteDisplayWith(obj?: Movie): string  {
+  movieAutoCompleteDisplayWith(obj?: Movie): string {
     return obj ? obj.title : '';
   }
-  
-  cineRoomAutoCompleteDisplayWith(obj?: CineRoom): string  {
+
+  cineRoomAutoCompleteDisplayWith(obj?: CineRoom): string {
     return obj ? obj.name : '';
   }
 
   RequireMatch(control: AbstractControl) {
     const selection: any = control.value;
     if (typeof selection === 'string') {
-        return { incorrect: true };
+      return { incorrect: true };
     }
     return null;
   }
-  
 }
-
