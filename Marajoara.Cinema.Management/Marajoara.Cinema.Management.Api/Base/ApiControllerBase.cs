df31c5e77cff802cfa241aba00cfd17d42093ec5
Rafael Polmann <rafaelpolmann@gmail.com>
2022-10-22 20:07:39 -0300

    Implemented validation to user permission in the API controllers.

--- Marajoara.Cinema.Management/Marajoara.Cinema.Management.Api/Base/ApiControllerBase.cs
+++ Marajoara.Cinema.Management/Marajoara.Cinema.Management.Api/Base/ApiControllerBase.cs
@@ -1,5 +1,7 @@
 ﻿using Marajoara.Cinema.Management.Api.Exceptions;
+using Marajoara.Cinema.Management.Api.Helpers;
 using Marajoara.Cinema.Management.Domain.Common.ResultModule;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Net;
@@ -8,16 +10,39 @@ namespace Marajoara.Cinema.Management.Api.Base
 {
     public class ApiControllerBase : Controller
     {
-        public IActionResult HandleResult<TFailure, TSuccess>(Result<TFailure, TSuccess> callback) where TFailure : Exception
+        private readonly IHttpContextAccessor _context;
+        public ApiControllerBase() { }
+
+        public ApiControllerBase(IHttpContextAccessor context)
         {
-            return callback.IsSuccess ? Ok(callback.Success) : HandleFailure(callback.Failure);
+            _context = context;
         }
 
-        public IActionResult HandleFailure<T>(T exceptionToHandle) where T : Exception
+        public IActionResult HandleResult<TFailure, TSuccess>(Result<TFailure, TSuccess> callback,
+                                                              HttpStatusCode statusCode = HttpStatusCode.InternalServerError) where TFailure : Exception
+        {
+            return callback.IsSuccess ? Ok(callback.Success) : HandleFailure(callback.Failure, statusCode);
+        }
+
+        public IActionResult HandleFailure<T>(T exceptionToHandle, HttpStatusCode statusCode) where T : Exception
         {
             ExceptionPayload exceptionPayload = ExceptionPayload.New(exceptionToHandle);
 
-            return StatusCode(HttpStatusCode.InternalServerError.GetHashCode(), exceptionPayload);
+            return StatusCode(statusCode.GetHashCode(), exceptionPayload);
+        }
+
+        protected Result<Exception, bool> ValitadeUserPermission(int userAccountID)
+        {
+            Result<Exception, bool> result = Result.Run(() =>
+            {
+                if (_context != null &&
+                    (ClaimsHelper.GetUserAccountID(_context) == userAccountID ||
+                     ClaimsHelper.GetRole(_context) == Domain.UserAccountModule.AccessLevel.Manager))
+                    return true;
+                else
+                    throw new Exception("User does not have permission to access those datas.");
+            });
+            return result;
         }
     }
 }
